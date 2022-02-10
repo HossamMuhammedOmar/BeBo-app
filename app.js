0a4f72f6e964da5123878ce6b972eaf8a18d779d
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-02-10 22:41:56 +0200

    active pads

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const kickTrack = document.querySelector("#kick-select");
-const snareTrack = document.querySelector("#snare-select");
-const hitHatTrack = document.querySelector("#hithat-select");
+class TrackKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickSound = document.querySelector(".kick-sound");
+    this.snareSound = document.querySelector(".snare-sound");
+    this.hihatSound = document.querySelector(".hihat-sound");
+    this.index = 0;
+    this.trackSpeed = 150;
+    this.play = document.querySelector(".play");
+  }
 
-kickTrack.addEventListener("click", (e) => {
-  console.log(e.target.value);
-});
+  repeat() {
+    let step = this.index % 8;
+    const activePad = document.querySelectorAll(`.p${step}`);
+    this.index++;
+  }
 
-// for (let option of selector) {
-//   console.log(option);
-// }
+  start() {
+    const interval = (60 / this.trackSpeed) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+
+  activeThePads() {
+    this.pads.forEach((pad) => {
+      pad.addEventListener("click", () => {
+        pad.classList.toggle("active");
+      });
+    });
+  }
+}
+
+const testTracks = new TrackKit();
+testTracks.play.addEventListener("click", () => testTracks.activeThePads());
