48c8c1b8f1c5d0431e811bba4fd6b0569fe08f62
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-02-11 21:55:57 +0200

    active new tracks

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ class TrackKit {
     this.snareSound = document.querySelector(".snare-sound");
     this.hihatSound = document.querySelector(".hihat-sound");
     this.index = 0;
-    this.trackSpeed = 150;
+    this.trackSpeed = 250;
     this.play = document.querySelector(".play");
+    this.isPlaying = null;
+    this.currentKick = "/sounds/kick-classic.wav";
+    this.currentSnare = "/sounds/snare-acoustic.wav";
+    this.currentHihat = "/sounds/hihat-acoustic.wav";
+    this.selects = document.querySelectorAll("select");
   }
 
   activeThePads() {
@@ -38,9 +43,41 @@ class TrackKit {
 
   start() {
     const interval = (60 / this.trackSpeed) * 1000;
-    setInterval(() => {
-      this.repeat();
-    }, interval);
+    if (!this.isPlaying) {
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
+    } else {
+      clearInterval(this.isPlaying);
+      this.isPlaying = null;
+    }
+  }
+
+  updateButtonText() {
+    if (!this.isPlaying) {
+      this.play.innerText = "STOP";
+      this.play.classList.add("active");
+    } else {
+      this.play.innerText = "PLAY";
+      this.play.classList.remove("active");
+    }
+  }
+
+  changeSound(e) {
+    const selectName = e.target.name;
+    const selectValue = e.target.value;
+    switch (selectName) {
+      case "kick-select":
+        this.kickSound.src = selectValue;
+        break;
+      case "snare-select":
+        this.snareSound.src = selectValue;
+        break;
+
+      case "hihat-select":
+        this.hihatSound.src = selectValue;
+        break;
+    }
   }
 }
 
@@ -53,4 +90,13 @@ track.pads.forEach((pad) => {
   });
 });
 
-track.play.addEventListener("click", () => track.start());
+track.play.addEventListener("click", () => {
+  track.updateButtonText();
+  track.start();
+});
+
+track.selects.forEach((select) => {
+  select.addEventListener("change", function (e) {
+    track.changeSound(e);
+  });
+});
